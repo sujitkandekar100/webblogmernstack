c015e7dd70585f7c1caadf0fcc85d3ac3a1ca90a
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-15 22:09:26 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -8,26 +8,26 @@ const BlogPostCard = ({ content, author }) => {
     let { fullname, profile_img, username } = author;
 
     return (
-        <Link to={`/blog/${id}`} className="flex flex-col border border-grey p-4 rounded-md mb-4 w-full max-w-[70%]">
+        <Link to={`/blog/${id}`} className="flex flex-col border border-grey p-2 rounded-md mb-4 w-full max-w-[50%]">
             <div className="w-full">
-                <img src={banner} className="w-full h-[10.5rem] rounded-md object-cover" alt="Blog Banner" />
+                <img src={banner} className="w-full h-24 rounded-md object-cover" alt="Blog Banner" />
             </div>
 
-            <div className="w-full mt-4">
+            <div className="w-full mt-2">
                 <div className="flex gap-2 items-center mb-2">
-                    <img src={profile_img} className="w-8 h-8 rounded-full" alt="Author" />
-                    <p className="line-clamp-1 font-semibold">{fullname} @{username}</p>
-                    <p className="min-w-fit text-sm text-grey">{getDay(publishedAt)}</p>
+                    <img src={profile_img} className="w-6 h-6 rounded-full" alt="Author" />
+                    <p className="line-clamp-1 font-semibold text-sm">{fullname} @{username}</p>
+                    <p className="min-w-fit text-xs text-grey">{getDay(publishedAt)}</p>
                 </div>
 
-                <h1 className="blog-title text-xl font-bold">{title}</h1>
+                <h1 className="blog-title text-lg font-bold">{title}</h1>
 
-                <p className="my-3 text-base font-gelasio leading-6 text-grey line-clamp-3">{des}</p>
+                <p className="my-2 text-sm font-gelasio leading-5 text-grey line-clamp-2">{des}</p>
 
-                <div className="flex gap-4 mt-4">
-                    <span className="btn-light py-1 px-4 rounded-md bg-light-grey text-dark-grey">{tags[0]}</span>
-                    <span className="ml-3 flex items-center gap-2 text-dark-grey">
-                        <i className="fi fi-rr-heart text-xl"></i>
+                <div className="flex gap-4 mt-2">
+                    <span className="btn-light py-1 px-3 rounded-md bg-light-grey text-dark-grey text-xs">{tags[0]}</span>
+                    <span className="ml-3 flex items-center gap-2 text-dark-grey text-xs">
+                        <i className="fi fi-rr-heart text-lg"></i>
                         {total_likes}
                     </span>
                 </div>
