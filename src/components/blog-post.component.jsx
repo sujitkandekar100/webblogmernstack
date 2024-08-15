1a86d1e6a5694e94bdc5519e99848958123c5208
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-15 22:02:11 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -8,9 +8,9 @@ const BlogPostCard = ({ content, author }) => {
     let { fullname, profile_img, username } = author;
 
     return (
-        <Link to={`/blog/${id}`} className="flex flex-col border border-grey p-4 rounded-md mb-4 w-full max-w-sm">
+        <Link to={`/blog/${id}`} className="flex flex-col border border-grey p-4 rounded-md mb-4 w-full max-w-[65%]">
             <div className="w-full">
-                <img src={banner} className="w-full h-48 rounded-md object-cover" alt="Blog Banner" />
+                <img src={banner} className="w-full h-40 rounded-md object-cover" alt="Blog Banner" />
             </div>
 
             <div className="w-full mt-4">
