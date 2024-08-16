15c13e1f1f3c995e7b1d0e9a156169152d44ccf3
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-16 21:42:20 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -7,7 +7,7 @@ const BlogPostCard = ({ content, author }) => {
     let { fullname, profile_img, username } = author;
 
     return ( 
-        <Link to={`/blog/${id}`} className="flex gap-8 items-center border-b border-grey pb-5 mb-4 hover:scale-105 hover:shadow-lg transition duration-300">
+        <Link to={`/blog/${id}`} className="flex gap-8 items-center border-b border-grey pb-5 mb-4 hover:scale-105  w-full max-w-[50%] hover:shadow-lg transition duration-300">
             <div className="w-full">
                 <div className="flex gap-2 items-center mb-7">
                     <img src={profile_img} className="w-8 h-8 rounded-full" alt={`${fullname}'s profile`} />
