ca5402ad5d4dcfa5ff52be47653ab8eb66cd9f37
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-16 14:37:05 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -3,34 +3,31 @@ import { getDay } from "../common/date";
 import { Link } from "react-router-dom";
 
 const BlogPostCard = ({ content, author }) => {
-
     let { publishedAt, tags, title, des, banner, activity: { total_likes }, blog_id: id } = content;
     let { fullname, profile_img, username } = author;
 
     return (
-        <Link to={`/blog/${id}`} className="flex flex-col md:flex-row gap-4 items-center border border-grey p-4 rounded-md mb-4 max-w-xl">
-            <div className="w-full md:w-1/2">
-                <img src={banner} className="w-full h-48 rounded-md object-cover" alt="Blog Banner" />
-            </div>
+        <Link to={`/blog/${id}`} className="flex flex-col gap-4 items-start border border-grey p-4 rounded-md mb-4">
+            <img src={banner} className="w-full h-48 rounded-md object-cover mb-4" alt="Blog Banner" />
             
-            <div className="w-full md:w-1/2">
-                <div className="flex gap-2 items-center mb-4">
-                    <img src={profile_img} className="w-8 h-8 rounded-full" alt="Author" />
+            <div className="flex gap-2 items-center mb-4">
+                <img src={profile_img} className="w-8 h-8 rounded-full" alt="Author" />
+                <div className="flex-1">
                     <p className="line-clamp-1 font-semibold">{fullname} @{username}</p>
-                    <p className="min-w-fit text-sm text-grey">{getDay(publishedAt)}</p>
+                    <p className="text-sm text-grey">{getDay(publishedAt)}</p>
                 </div>
+            </div>
 
-                <h1 className="blog-title text-xl font-bold">{title}</h1>
+            <h1 className="blog-title text-lg font-bold">{title}</h1>
 
-                <p className="my-3 text-base font-gelasio leading-6 text-grey line-clamp-3">{des}</p>
+            <p className="my-3 text-sm text-grey line-clamp-3">{des}</p>
 
-                <div className="flex gap-4 mt-4">
-                    <span className="btn-light py-1 px-4 rounded-md bg-light-grey text-dark-grey">{tags[0]}</span>
-                    <span className="ml-3 flex items-center gap-2 text-dark-grey">
-                        <i className="fi fi-rr-heart text-xl"></i>
-                        {total_likes}
-                    </span>
-                </div>
+            <div className="flex justify-between items-center w-full mt-4">
+                <span className="btn-light py-1 px-3 rounded-md bg-light-grey text-dark-grey">{tags[0]}</span>
+                <span className="flex items-center gap-2 text-dark-grey">
+                    <i className="fi fi-rr-heart text-xl"></i>
+                    {total_likes}
+                </span>
             </div>
         </Link>
     )
