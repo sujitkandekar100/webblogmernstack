fbe5c0b8f0e5f0afd6112156024ba47bd838b5d2
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-17 18:08:31 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -4,15 +4,19 @@ const BlogPostCard = ({ content, author }) => {
     let { title, des, banner, tags, activity: { total_likes }, blog_id: id } = content;
 
     return (
-        <Link to={`/blog/${id}`} className="w-[274px] shadow-md rounded-lg bg-white flex flex-col items-start justify-start p-4">
+        <Link to={`/blog/${id}`} className="w-full shadow-md rounded-lg bg-white flex flex-col items-start justify-start">
+            {/* Image Section */}
             <img
-                className="w-full h-40 rounded-md object-cover"
+                className="w-full h-56 rounded-t-lg object-cover"
                 loading="lazy"
                 alt={title}
                 src={banner}
             />
-            <div className="mt-4 flex flex-col">
+            
+            {/* Content Section */}
+            <div className="p-4 w-full">
                 <b className="text-lg font-bold">{title}</b>
+                
                 <div className="flex items-center text-gray-600 mt-2">
                     <span className="text-sm font-medium">{`4.1 `}</span>
                     <img
@@ -25,7 +29,9 @@ const BlogPostCard = ({ content, author }) => {
                         Free
                     </div>
                 </div>
+                
                 <p className="text-sm text-gray-700 mt-2">{des}</p>
+                
                 <button className="mt-4 py-2 px-4 bg-gray-100 text-black rounded-lg self-center">
                     Try it out
                 </button>
