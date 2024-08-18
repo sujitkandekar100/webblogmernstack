7f3f7281ebd08b00b421860e8834db8e65a100a2
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-18 16:52:59 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -20,7 +20,7 @@ const BlogPostCard = ({ content, author }) => {
                 <div>
                     <b className="text-lg font-bold block leading-tight">{title}</b>                 
                     <div className="flex items-center text-gray-600 mt-2">
-                        <i className="fi fi-rr-heart text-xl"></i>
+                        <i class="fi fi-rr-social-network text-xl"></i>
                         ({total_likes}) 
                         <div className="ml-auto rounded bg-gray-200 text-gray-800 px-2 py-1 text-sm">
                             Free
