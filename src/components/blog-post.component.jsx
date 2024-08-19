ae2d7cc0ac22841270ac9311d57c6f6f7c6df66a
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-19 14:54:54 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -22,6 +22,12 @@ const BlogPostCard = ({ content, author }) => {
                     <b className="text-lg font-bold block leading-tight">{title}</b>
                     <span className="btn-light py-1 px-4">{tags[0]}</span>
                      </div>
+                    <div className="flex gap-4 mt-7">
+                      <b className="text-lg font-bold block leading-tight">{title}</b>
+                    <span className="ml-3 flex items-center gap-2 text-dark-grey">
+                        <span className="btn-light py-1 px-4">{tags[0]}</span>
+                    </span>
+                </div>
                     <div className="flex items-center text-gray-600 mt-2">
                         <i class="fi fi-rs-social-network text-xl"></i>
                         ({total_likes}) 
