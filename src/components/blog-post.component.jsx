79e2829eb9f3811e1c352b1bccd014e44d41cbc3
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-20 11:23:23 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -26,7 +26,7 @@ const BlogPostCard = ({ content, author }) => {
                     <div className="flex items-center text-gray-600 mt-2">
                        
                    <span className="ml-3 flex items-center gap-2 text-dark-grey">
-                        <span className="btn-light py-1 text-xs px-2 ">{tags[0]}</span>
+                        <span className="btn-light py-1 text-sm px-2 ">{tags[0]}</span>
                     </span>
                         <div className="ml-auto btn-light rounded bg-gray-200 text-gray-800 px-2 py-1 text-sm">
                             Free
