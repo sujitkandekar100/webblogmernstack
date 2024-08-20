7e01eb257e7a00e78303f9a7d69c99236229c797
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-20 22:10:14 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -28,7 +28,7 @@ const BlogPostCard = ({ content, author }) => {
                    <span className=" flex items-center gap-2 text-dark-grey">
                         <span className="btn-light py-1 text-sm px-2 ">{tags[0]}</span>
                     </span>
-                      <div className="ml-auto btn-light rounded bg-gray-200 text-gray-800 px-2 py-1" style={{ fontSize: '10px' }}>
+                      <div className="ml-auto btn-light rounded bg-gray-200  px-2 py-1">
                           3 Demo
                        </div>
 
