fedffc1a98dcb4c254e2f331ab0fdf6c527ee23d
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-20 10:02:16 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -7,6 +7,7 @@ const BlogPostCard = ({ content, author }) => {
         <Link 
             to={`/blog/${id}`} 
             className="shadow-md rounded-lg bg-white flex flex-col text-left text-[14px] text-[#000]">
+            
             {/* Image Section */}
             <img
                 className="w-full h-40 sm:h-48 rounded-t-lg object-cover"
@@ -17,29 +18,33 @@ const BlogPostCard = ({ content, author }) => {
             
             {/* Content Section */}
             <div className="p-4 flex flex-col justify-between h-full">
-                <div>  
-                    <div className="flex gap-2 mt-1">
-                      <b className="text-lg font-bold block leading-tight">{title}</b>
-                       <i class="fi fi-rs-social-network text-xl "></i>
-                        ({total_likes}) 
-                </div>
-                    <div className="flex items-center text-gray-600 mt-2">
-                       
-                   <span className="ml-3 flex items-center gap-2 text-dark-grey">
-                        <span className="btn-light py-1 text-xs px-2 ">{tags[0]}</span>
-                    </span>
-                        <div className="ml-auto btn-light rounded bg-gray-200 text-gray-800 px-2 py-1 text-sm">
-                            Free
-                        </div>
+                
+                {/* Free Badge */}
+                <div className="flex justify-end mb-2">
+                    <div className="btn-light rounded bg-gray-200 text-gray-800 px-2 py-1 text-xs">
+                        Free
                     </div>
-                    
-                    <p className=" my-3 text-xl font-gelasio leading-7 max-sm:hidden md:max-[1100px]:hidden line-clamp-2">{des}</p>
                 </div>
                 
+                {/* Title and Likes */}
+                <div className="flex justify-between items-center">
+                    <b className="text-xl font-bold block leading-tight">{title}</b>
+                    <div className="flex items-center text-gray-600">
+                        <i className="fi fi-rs-social-network text-xl"></i>
+                        <span className="ml-1">({total_likes})</span>
+                    </div>
+                </div>
                 
-            </div>
-           
+                {/* Tags */}
+                <div className="flex items-center mt-2">
+                    <span className="btn-light py-1 text-xs px-2 text-dark-grey">{tags[0]}</span>
+                </div>
 
+                {/* Description */}
+                <p className="my-3 text-base font-gelasio leading-6 line-clamp-2">
+                    {des}
+                </p>
+            </div>
         </Link>
     );
 };
