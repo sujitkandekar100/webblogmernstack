cba17319979d255d55f80de545c451ad8319199a
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-21 12:21:47 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -21,8 +21,7 @@ const BlogPostCard = ({ content, author }) => {
                 <div>  
                     <div className="flex gap-2 mt-1">
                         <b className="text-xl font-bold block leading-tight">{title}</b>
-                        <i class="fi fi-sr-caret-up "></i>
-                        ({total_likes}) 
+                        <i class="fi fi-sr-caret-up "></i> ({total_likes}) 
                     </div>
                     <div className="flex justify-between text-gray-600 mt-2">
                         <span className="flex  gap-2 text-dark-grey ">
