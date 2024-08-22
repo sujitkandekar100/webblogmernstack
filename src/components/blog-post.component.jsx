36625ee6abd5368c774d6ca677ac1b2452b8d2c8
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-22 10:11:28 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -1,47 +1,41 @@
+/* eslint-disable react/prop-types */
+import { getDay } from "../common/date";
 import { Link } from "react-router-dom";
 
 const BlogPostCard = ({ content, author }) => {
-    let { title, des, banner, tags, activity: { total_likes }, blog_id: id } = content;
 
-    return (
-        <Link 
-            to={`/blog/${id}`} 
-            className="shadow-md rounded-lg flex flex-col text-left text-[14px] min-h-[100px]" // Fixed card height
-        >
-            {/* Image Section */}
-            <img
-                className="w-full h-40 sm:h-48 rounded-t-lg object-cover"
-                loading="lazy"
-                alt={title}
-                src={banner}
-            />
-            
-            {/* Content Section */}
-            <div className="p-4 flex flex-col justify-end h-full overflow-hidden">
-                <div>  
-     <div className="flex gap-2 mt-1">
-    <b className="text-xl font-bold block leading-tight">{title}</b>
-    <div className="flex items-center space-x-1">
-        <i className="fi fi-sr-caret-up text-2xl"></i>
-        <span className="leading-none">({total_likes})</span>
-    </div>
-</div>
+    let { publishedAt, tags, title, des, banner, activity: { total_likes }, blog_id: id } = content;
+    let { fullname, profile_img, username } = author;
 
-
-                    <div className="flex justify-between text-gray-600 mt-2">
-                        <span className="flex  gap-2 text-dark-grey ">
-                            <span className="btn-light py-1 text-sm px-2">{tags[0]}</span>
-                        </span>
-                        <span className="btn-light py-1 text-sm px-2">3 Demo</span>
-                    </div>
-                    
-                    <p className="my-3 text-base font-gelasio leading-6 line-clamp-3 overflow-hidden text-ellipsis">
-                        {des}
-                    </p>
+    return ( 
+        <Link to={`/blog/${id}`} className="flex gap-8 items-center border-b border-grey pb-5 mb-4">
+            <div className="w-full">
+                <div className="flex gap-2 items-center mb-7">
+                    <img src={profile_img} className="w-6 h-6 rounded-full" />
+                    <p className="line-clamp-1">{fullname} @{username}</p>
+                    <p className="min-w-fit">{ getDay(publishedAt) }</p>
                 </div>
+
+                <h1 className="blog-title">{title}</h1>
+
+                <p className="my-3 text-xl font-gelasio leading-7 max-sm:hidden md:max-[1100px]:hidden line-clamp-2">{des}</p>
+
+                <div className="flex gap-4 mt-7">
+                    <span className="btn-light py-1 px-4">{tags[0]}</span>
+                    <span className="ml-3 flex items-center gap-2 text-dark-grey">
+                        <i className="fi fi-rr-heart text-xl"></i>
+                        { total_likes }
+                    </span>
+                </div>
+
             </div>
+            
+            <div className="h-28 aspect-sqaure bg-grey">
+                <img src={banner} className="w-full h-full aspect-square object-cover" />
+            </div>
+
         </Link>
-    );
-};
+    )
+}
 
 export default BlogPostCard;
