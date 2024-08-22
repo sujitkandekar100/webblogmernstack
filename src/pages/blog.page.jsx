d47d055e2b4436673163860718b5f98b588ef1cc
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-22 13:51:55 +0530

    Update blog.page.jsx

--- src/pages/blog.page.jsx
+++ src/pages/blog.page.jsx
@@ -3,7 +3,6 @@ import { createContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
 import Loader from "../components/loader.component";
-import { getDay } from "../common/date";
 import BlogInteraction from "../components/blog-interaction.component";
 import BlogPostCard from "../components/blog-post.component";
 import BlogContent from "../components/blog-content.component";
@@ -13,9 +12,8 @@ export const blogStructure = {
     title: '',
     des: '',
     conent: [],
-    author: { personal_info: { } },
     banner: '',
-    publishedAt: '',
+  
 }
 
 export const BlogContext = createContext({ });
@@ -31,7 +29,7 @@ const BlogPage = () => {
     const [ commentsWrapper, setCommentsWrapper ] = useState(false);
     const [ totalParentCommentsLoaded, setTotalParentCommentsLoaded ] = useState(0);
 
-    let { title, content, banner, author: { personal_info: { fullname, username: author_username , profile_img } }, publishedAt } = blog;
+    let { title, content, banner } = blog;
 
     const fetchBlog = () => {
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
@@ -86,23 +84,7 @@ const BlogPage = () => {
 
                         <div className="mt-12">
                             <h2>{title}</h2>
-
-                            <div className="flex max-sm:flex-col justify-between my-8">
-                                <div className="flex gap-5 items-start">
-                                    <img src={profile_img} className="w-12 h-12 rounded-full" />
-
-                                    <p className="capitalize">
-                                        {fullname}
-                                        <br />
-                                        @
-                                        <Link to={`/user/${author_username}`} className="underline">{author_username}</Link>
-                                    </p>
-                                    
-                                </div>
-                                <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">Published on {getDay(publishedAt)}</p>
-                            </div>
-                        </div>
-
+                            
                         <BlogInteraction />
 
                         <div className="my-12 font-gelasio blog-page-content">
