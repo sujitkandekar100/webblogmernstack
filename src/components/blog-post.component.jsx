6e9e9d9edb8d72ce673f309317e636539ee38b1a
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-23 13:50:01 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -17,7 +17,7 @@ const BlogPostCard = ({ content, author }) => {
      <div className="flex gap-2 mt-1">
     <b className="text-xl font-bold block leading-tight">{title}</b>
     <div className="flex items-center space-x-1">
-        <i className="fi fi-sr-caret-up text-2xl"></i>
+        <i className="fi fi-rr-caret-up text-2xl"></i>
         <span className="leading-none">({total_likes})</span>
     </div>
 </div>
