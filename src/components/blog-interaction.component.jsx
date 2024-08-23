eb85ca0e81a0922b148c268a95265f750058dc23
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-23 18:49:23 +0530

    Update blog-interaction.component.jsx

--- src/components/blog-interaction.component.jsx
+++ src/components/blog-interaction.component.jsx
@@ -90,7 +90,7 @@ const BlogInteraction = () => {
                         <i className="fi fi-brands-linkedin text-xl hover:text-linkedin"></i>
                     </Link>
                     <Link to={`https://twitter.com/intent/tweet?text=Read ${encodeURIComponent(title)}&url=${encodeURIComponent(location.href)}`}>
-                        <i className="fi fi-brands-twitter text-xl hover:text-twitter"></i>
+                        <i className="fi fi-brands-twitter-alt-square text-xl hover:text-twitter"></i>
                     </Link>
                 </div>
             </div>
