12897fc1b5ca3ccd7c5be1a34a79e25178d36245
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-23 11:00:23 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -5,36 +5,37 @@ const BlogPostCard = ({ content, author }) => {
 
     return (
         <Link 
-            to={`/blog/${id}`} 
-            className="shadow-md rounded-lg flex flex-col text-left text-[14px] w-64 h-96" // Fixed card width and height
+            to={/blog/${id}} 
+            className="shadow-md rounded-lg flex flex-col text-left text-[14px] min-h-[100px]" // Fixed card height
         >
             {/* Image Section */}
             <img
-                className="w-full h-40 rounded-t-lg object-cover" // Fixed image height
+                className="w-full h-40 sm:h-48 rounded-t-lg object-cover"
                 loading="lazy"
                 alt={title}
                 src={banner}
             />
             
             {/* Content Section */}
-            <div className="p-4 flex flex-col justify-between h-full">
+            <div className="p-4 flex flex-col justify-end h-full overflow-hidden">
                 <div>  
-                    <div className="flex gap-2 mt-1">
-                        <b className="text-lg font-bold block leading-tight">{title}</b>
-                        <div className="flex items-center space-x-1">
-                            <i className="fi fi-sr-caret-up text-xl"></i>
-                            <span className="leading-none">({total_likes})</span>
-                        </div>
-                    </div>
+     <div className="flex gap-2 mt-1">
+    <b className="text-xl font-bold block leading-tight">{title}</b>
+    <div className="flex items-center space-x-1">
+        <i className="fi fi-sr-caret-up text-2xl"></i>
+        <span className="leading-none">({total_likes})</span>
+    </div>
+</div>
+
 
                     <div className="flex justify-between text-gray-600 mt-2">
-                        <span className="flex gap-2 text-dark-grey">
+                        <span className="flex  gap-2 text-dark-grey ">
                             <span className="btn-light py-1 text-sm px-2">{tags[0]}</span>
                         </span>
                         <span className="btn-light py-1 text-sm px-2">3 Demo</span>
                     </div>
                     
-                    <p className="my-3 text-sm leading-5 line-clamp-3 overflow-hidden ">
+                    <p className="my-3 text-base font-gelasio leading-6 line-clamp-3 overflow-hidden text-ellipsis">
                         {des}
                     </p>
                 </div>
@@ -43,4 +44,4 @@ const BlogPostCard = ({ content, author }) => {
     );
 };
 
-export default BlogPostCard;
+export default BlogPostCard; 
