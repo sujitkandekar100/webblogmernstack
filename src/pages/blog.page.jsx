278bb6c2b0b1d8849810ab7e9d9c271b43c7084d
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-23 08:11:33 +0530

    Update blog.page.jsx

--- src/pages/blog.page.jsx
+++ src/pages/blog.page.jsx
@@ -31,7 +31,7 @@ const BlogPage = () => {
     const [ commentsWrapper, setCommentsWrapper ] = useState(false);
     const [ totalParentCommentsLoaded, setTotalParentCommentsLoaded ] = useState(0);
 
-    let { title, content, banner, author: { personal_info: { fullname, username: author_username , profile_img } }, publishedAt } = blog;
+    let { title, content, banner} = blog;
 
     const fetchBlog = () => {
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
@@ -86,21 +86,6 @@ const BlogPage = () => {
 
                         <div className="mt-12">
                             <h2>{title}</h2>
-
-                            <div className="flex max-sm:flex-col justify-between my-8">
-                                <div className="flex gap-5 items-start">
-                                    <img src={profile_img} className="w-12 h-12 rounded-full" />
-
-                                    <p className="capitalize">
-                                        {fullname}
-                                        <br />
-                                        @
-                                        <Link to={`/user/${author_username}`} className="underline">{author_username}</Link>
-                                    </p>
-                                    
-                                </div>
-                                <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">Published on {getDay(publishedAt)}</p>
-                            </div>
                         </div>
 
                         <BlogInteraction />
@@ -128,7 +113,7 @@ const BlogPage = () => {
                                             let { author: { personal_info } } = blog;
 
                                             return <AnimationWrapper key={i} transition={{ duration: 1, delay: i*0.08 }}>
-                                                <BlogPostCard content={blog} author={personal_info} />
+                                                <BlogPostCard content={blog}  />
                                             </AnimationWrapper>
 
                                         })
