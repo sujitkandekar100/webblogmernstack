91a6e20f87c8645e1e70c8ec4d6e5fc94348c3f5
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-23 08:36:35 +0530

    Update blog-interaction.component.jsx

--- src/components/blog-interaction.component.jsx
+++ src/components/blog-interaction.component.jsx
@@ -2,63 +2,75 @@ import { useContext, useEffect } from "react";
 import { BlogContext } from "../pages/blog.page";
 import { Link } from "react-router-dom";
 import { UserContext } from "../App";
-import { Toaster, toast } from "react-hot-toast"
+import { Toaster, toast } from "react-hot-toast";
 import axios from "axios";
 
 const BlogInteraction = () => {
+    let {
+        blog,
+        blog: {
+            _id,
+            title,
+            blog_id,
+            banner,
+            activity,
+            activity: { total_likes, total_comments },
+            author: { personal_info: { username: author_username } },
+        },
+        setBlog,
+        islikedByUser,
+        setLikedByUser,
+        setCommentsWrapper,
+    } = useContext(BlogContext);
 
-    let { blog, blog: { _id, title, blog_id,banner, activity, activity: { total_likes, total_comments }, author: { personal_info: { username: author_username } }  }, setBlog, islikedByUser, setLikedByUser, setCommentsWrapper } = useContext(BlogContext);
-
-    let { userAuth: { username, access_token } } = useContext(UserContext);
+    let {
+        userAuth: { username, access_token },
+    } = useContext(UserContext);
 
     useEffect(() => {
-
-        if( access_token ){
-            // make request to server to get like information
-            axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/isliked-by-user", { _id }, {
-                headers: {
-                    'Authorization': `Bearer ${access_token}`
-                }
-            })
-            .then(({ data: { result } }) => {
-                setLikedByUser(Boolean(result))
-            })
-            .catch(err => {
-                console.log(err);
-            })
+        if (access_token) {
+            // Make request to server to get like information
+            axios
+                .post(import.meta.env.VITE_SERVER_DOMAIN + "/isliked-by-user", { _id }, {
+                    headers: {
+                        Authorization: `Bearer ${access_token}`,
+                    },
+                })
+                .then(({ data: { result } }) => {
+                    setLikedByUser(Boolean(result));
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         }
-
-    }, [])
+    }, []);
 
     const handleLike = () => {
-
-        if(access_token){
-            // like the blog
-            setLikedByUser(preVal => !preVal);
+        if (access_token) {
+            // Like the blog
+            setLikedByUser((preVal) => !preVal);
 
             !islikedByUser ? total_likes++ : total_likes--;
 
-            setBlog({ ...blog, activity: { ...activity, total_likes } })
+            setBlog({ ...blog, activity: { ...activity, total_likes } });
 
-            axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/like-blog", { _id, islikedByUser }, {
-                headers: { 
-                    'Authorization': `Bearer ${access_token}`
-                }
-            })
-            .then(({ data }) => {
-                console.log(data);
-            })
-            .catch(err =>{
-                console.log(err);
-            })
-            
-        } 
-        else{
-            // not logged in
-            toast.error("please login to like this blog")
+            axios
+                .post(import.meta.env.VITE_SERVER_DOMAIN + "/like-blog", { _id, islikedByUser }, {
+                    headers: {
+                        Authorization: `Bearer ${access_token}`,
+                    },
+                })
+                .then(({ data }) => {
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        } else {
+            // Not logged in
+            toast.error("Please login to like this blog");
         }
-
-    }
+    };
 
     return (
         <>
@@ -69,35 +81,47 @@ const BlogInteraction = () => {
                 <div className="flex gap-3 items-center">
                     <button
                         onClick={handleLike}
-                        className={"w-10 h-10 rounded-full flex items-center justify-center " + ( islikedByUser ? "bg-red/20 text-red" : "bg-grey/80" )}
+                        className={
+                            "w-10 h-10 rounded-full flex items-center justify-center " +
+                            (islikedByUser ? "bg-red/20 text-red" : "bg-grey/80")
+                        }
                     >
-                        <i className={"fi " + ( islikedByUser ? "fi-sr-heart" : "fi-rr-heart" )}></i>
+                        <i className={"fi " + (islikedByUser ? "fi-sr-heart" : "fi-rr-heart")}></i>
                     </button>
-                    <p className="text-xl text-dark-grey">{ total_likes }</p>
+                    <p className="text-xl text-dark-grey">{total_likes}</p>
 
                     <button
-                        onClick={() => setCommentsWrapper(preVal => !preVal)}
+                        onClick={() => setCommentsWrapper((preVal) => !preVal)}
                         className="w-10 h-10 rounded-full flex items-center justify-center bg-grey/80"
                     >
                         <i className="fi fi-rr-comment-dots"></i>
                     </button>
-                    <p className="text-xl text-dark-grey">{ total_comments }</p>
+                    <p className="text-xl text-dark-grey">{total_comments}</p>
                 </div>
 
                 <div className="flex gap-6 items-center">
+                    {username === author_username ? (
+                        <Link to={`/editor/${blog_id}`} className="underline hover:text-purple">
+                            Edit
+                        </Link>
+                    ) : (
+                        ""
+                    )}
 
-                    {
-                        username == author_username ? 
-                        <Link to={`/editor/${blog_id}`} className="underline hover:text-purple">Edit</Link> : ""
-                    }
-
-                    <Link to={`https://twitter.com/intent/tweet?text=Read ${title}&img=${banner}&url=${location.href}`}><i className="fi fi-brands-twitter text-xl hover:text-twitter"></i></Link>
+                    {/* Twitter Share Button with Banner Image */}
+                    <a
+                        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`Read ${title}`)}&url=${encodeURIComponent(window.location.href)}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fi fi-brands-twitter text-xl hover:text-twitter"></i>
+                    </a>
                 </div>
             </div>
 
             <hr className="border-grey my-2" />
         </>
-    )
-}
+    );
+};
 
 export default BlogInteraction;
