95f8f8498e3db92ebac6194b47886b94bac6d0d6
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-24 18:34:42 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -4,7 +4,7 @@ import { getDay } from "../common/date";
 
 const MinimalBlogPost = ({ blog, index }) => {
     
-    let { title, blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
+    let { title,banner,total_likes,tags, blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
 
     return (
         <Link to={`/blog/${id}`} className="flex gap-5 mb-8">
