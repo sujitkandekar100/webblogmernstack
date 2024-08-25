45170f03247e91fe49fb24ce336b03d4b1d15cf4
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-25 08:22:08 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -12,7 +12,7 @@ const MinimalBlogPost = ({ blog, index }) => {
 
             <div>
                 <div className="flex gap-2 items-center mb-7">
-                    <img src={profile_img} className="w-6 h-6 rounded-full" />
+                    <img src={profile_img} className="w-16 h-16 rounded-full" />
                     <p className="line-clamp-1">{fullname} @{username}</p>
                     <p className="min-w-fit">{ getDay(publishedAt) }</p>
                 </div>
