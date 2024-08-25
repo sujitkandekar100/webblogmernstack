c46dacce274037ec408820ce5027f95f61b48e95
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-25 21:36:18 +0530

    Update home.page.jsx

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -170,7 +170,7 @@ const HomePage = () => {
                 </div>
 
                 {/* filters and trending blogs */}
-                <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l wrap border-grey pl-8 pt-3 max-md:hidden">
+                <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l  border-grey pl-8 pt-3 max-md:hidden">
                     <div className="flex flex-col gap-10">
                         <div>
                             <h1 className="font-medium text-xl mb-8">
