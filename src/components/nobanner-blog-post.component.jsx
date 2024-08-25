169308d17cd2ad2f789524d98d1887970c3506c1
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-25 09:17:27 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -4,7 +4,7 @@ import { getDay } from "../common/date";
 
 const MinimalBlogPost = ({ blog, index }) => {
     
-    let { title,banner,total_likes,tags, blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
+    let { title, blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
 
     return (
         <Link to={`/blog/${id}`} className="flex gap-5 mb-8">
@@ -12,7 +12,7 @@ const MinimalBlogPost = ({ blog, index }) => {
 
             <div>
                 <div className="flex gap-2 items-center mb-7">
-                    <img src={banner} className="w-16 h-16 rounded-lg" />
+                    <img src={profile_img} className="w-6 h-6 rounded-full" />
                     <p className="line-clamp-1">{fullname} @{username}</p>
                     <p className="min-w-fit">{ getDay(publishedAt) }</p>
                 </div>
