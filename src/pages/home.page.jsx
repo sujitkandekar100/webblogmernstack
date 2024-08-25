27cb8074486264148d8eb8b5570fa85e6f75dba7
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-25 21:29:26 +0530

    Update home.page.jsx

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -109,7 +109,7 @@ const HomePage = () => {
 
     return (
         <AnimationWrapper>
-            <section className="h-cover flex justify-center gap-10">
+            <section className="h-cover flex flex-col gap-10">
                 {/* latest blogs */}
                 <div className="w-full">
                     <InPageNavigation
