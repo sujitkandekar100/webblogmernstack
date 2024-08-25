7f624b74ce090f5e284322955a065f92c10ec8c1
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-25 22:25:05 +0530

    Update home.page.jsx

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -111,7 +111,7 @@ const HomePage = () => {
         <AnimationWrapper>
             <section className="h-cover flex flex-col gap-10">
                 {/* latest blogs */}
-                <div className="w-full flex-wrap  ">
+                <div className="w-full wrap  ">
                     <InPageNavigation
                         routes={[ pageState , "trending blogs"]}
                         defaultHidden={[]}
