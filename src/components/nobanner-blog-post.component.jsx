3777a4514dcfe7f3cb60fbf9a5ebd7d930ccc516
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-26 15:13:05 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -23,21 +23,21 @@ const MinimalBlogPost = ({ blog, index }) => {
 
                 <div className="flex-1">
                     <div className="flex items-center justify-between gap-3">
-                        <h6 className="text-lg font-semibold text-xl">{title}</h6>
+                        <h5 className="text-lg font-semibold text-xl">{title}</h5>
                             <span className="btn-light  rounded-full  py-1 text-sm px-2">
                                 {tags[1]}
                             </span>
-                    
-                    </div>
+                    </div>           
                 </div>
-                <div className="flex justify-between text-gray-600 mt-2">
-                        <span className="flex  gap-2 text-dark-grey ">
-                             <div className="flex items-center space-x-1">
-        <i className="fi fi-ss-arrow-circle-up text-base "></i>
-        <span className="leading-none">({total_likes})</span>
-    </div>
-                        <span className=" btn-light py-1 text-sm px-2">{tags[1]}</span>
-                    </div>
+                  <div className="flex-2">
+                    <div className="flex items-center justify-between gap-3">
+                            <span className="btn-light  rounded-full  py-1 text-sm px-2">
+                                {tags[1]}
+                            </span>
+                    </div>           
+                </div>        
+       
+                
             </div>
         </Link>
     );
