778ccc65c46acb1822b6f42876af40dfe545fee9
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-26 13:25:46 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -10,20 +10,17 @@ const MinimalBlogPost = ({ blog, index }) => {
         <Link to={`/blog/${id}`} className="flex gap-5 mb-8">
             <h1 className="blog-index">{ index < 10 ? "0" + (index + 1) : index}</h1>
 
-            <div>
+            
                 <div className="flex gap-2  mb-7">
                     <img src={banner} className="w-16 h-16 rounded-md" />
                      <h1 className="blog-title text-x1">{title}</h1>
+                    </div>
         <div className="flex items-center space-x-1 line-clamp-2">
         <i className="fi fi-ss-arrow-circle-up text-base "></i>
         <span className="leading-none">({total_likes})</span>
     </div>
- 
-
-                </div>
 
                
-            </div>
         </Link>
     )
 }
