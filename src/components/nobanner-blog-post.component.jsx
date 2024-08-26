f7e32410a17b6220baf130a8d720600e6f33691a
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-26 14:08:21 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -20,9 +20,6 @@ const MinimalBlogPost = ({ blog, index }) => {
       <span className="btn-light bg-gray-200 text-gray-700 py-1 px-3 rounded-full text-sm">{tags[1]}</span>
     </div>
 
-    <div className="flex items-center text-sm text-gray-600 mt-1">
-      <span className="mr-1">{total_likes}</span>
-        <i className="fi fi-ss-arrow-circle-up text-base "></i>
     </div>
   </div>
 </div>
