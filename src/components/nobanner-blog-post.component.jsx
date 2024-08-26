123e117d03dee9cc9ab562bf8a14b83a92e71fa7
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-26 13:59:00 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -11,19 +11,19 @@ const MinimalBlogPost = ({ blog, index }) => {
             <h1 className="blog-index">{ index < 10 ? "0" + (index + 1) : index}</h1>
 
             
-              <div className="flex items-start gap-4 mb-4 p-4 bg-white rounded-lg shadow-md">
+              <div className="flex items-start gap-4 mb-4 p-4  rounded-lg shadow-md">
   <img src={banner} className="w-16 h-16 rounded-lg" />
 
   <div className="flex-1">
     <div className="flex items-center justify-between">
       <h1 className="text-lg font-semibold">{title}</h1>
-      <span className="btn-light bg-gray-200 text-gray-700 py-1 px-3 rounded-full text-sm">Free</span>
+      <span className="btn-light bg-gray-200 text-gray-700 py-1 px-3 rounded-full text-sm">{tags[1]}</span>
     </div>
 
     <div className="flex items-center text-sm text-gray-600 mt-1">
-      <span className="mr-1">4.2</span>
-      <i className="fi fi-ss-star text-xs text-yellow-500"></i>
-      <span className="ml-2">Entertainment</span>
+      <span className="mr-1">{total_likes}</span>
+        <i className="fi fi-ss-arrow-circle-up text-base "></i>
+      <span className="ml-2">{tags[0]}</span>
     </div>
   </div>
 </div>
