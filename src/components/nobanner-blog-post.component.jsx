571822e2d37dbde38109efec425d57746673bd8a
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-26 13:04:09 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -16,15 +16,7 @@ const MinimalBlogPost = ({ blog, index }) => {
                     <div className="line-clamp-1">
                         <h1 className="blog-title ">{title}</h1>
                     </div>
-                    <div className="flex justify-between text-gray-600 mt-2">
-  <div className="flex gap-2 text-dark-grey">
-    <div className="flex items-center space-x-1">
-      <i className="fi fi-ss-arrow-circle-up text-base"></i>
-      <span className="leading-none">({total_likes})</span>
-    </div>
-    <span className="btn-light py-1 text-sm px-2">{tags[1]}</span>
-  </div>
-</div>
+                    
 
                 </div>
 
