13bdeeda4e1b667aad6d07518f3c55fcb2f38274
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-08-26 12:59:54 +0530

    Update nobanner-blog-post.component.jsx

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -4,7 +4,7 @@ import { getDay } from "../common/date";
 
 const MinimalBlogPost = ({ blog, index }) => {
     
-    let { title, banner,blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
+    let { title, banner,activity: { total_likes },blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
 
     return (
         <Link to={`/blog/${id}`} className="flex gap-5 mb-8">
@@ -12,13 +12,23 @@ const MinimalBlogPost = ({ blog, index }) => {
 
             <div>
                 <div className="flex gap-2 items-center mb-7">
-                    <img src={banner} className="w-16 h-16 rounded-full" />
-                    <img src={profile_img} className="w-6 h-6 rounded-full" />
-                    <p className="line-clamp-1">{fullname} @{username}</p>
-                    <p className="min-w-fit">{ getDay(publishedAt) }</p>
+                    <img src={banner} className="w-16 h-16 squared-full" />
+                    <div className="line-clamp-1">
+                        <h1 className="blog-title ">{title}</h1>
+                    </div>
+                    <div className="flex justify-between text-gray-600 mt-2">
+  <div className="flex gap-2 text-dark-grey">
+    <div className="flex items-center space-x-1">
+      <i className="fi fi-ss-arrow-circle-up text-base"></i>
+      <span className="leading-none">({total_likes})</span>
+    </div>
+    <span className="btn-light py-1 text-sm px-2">{tags[1]}</span>
+  </div>
+</div>
+
                 </div>
 
-                <h1 className="blog-title">{title}</h1>
+               
             </div>
         </Link>
     )
