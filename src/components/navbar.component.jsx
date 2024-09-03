ba40e6e391c6ab7c8d6d07d396db62318d204105
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-09-03 13:49:33 +0530

    Update navbar.component.jsx

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -9,7 +9,6 @@ import { storeInSession } from "../common/session";
 
 const Navbar = () => {
 
-    const [ searchBoxVisibility, setSearchBoxVisibility ] = useState(false)
     const [ userNavPanel, setUserNavPanel ] = useState(false);
 
     let { theme, setTheme } = useContext(ThemeContext);
@@ -34,18 +33,10 @@ const Navbar = () => {
             })
         }
 
-    }, [access_token])
+    }, [access_token]);
 
     const handleUserNavPanel = () => {
         setUserNavPanel(currentVal => !currentVal);
-    }
-
-    const handleSearch = (e) => {
-        let query = e.target.value;
-        
-        if(e.keyCode == 13 && query.length){
-            navigate(`/search/${query}`);
-        }
     }
 
     const handleBlur = () => {
@@ -55,7 +46,6 @@ const Navbar = () => {
     }
 
     const changeTheme = () => {
-        
         let newTheme = theme == "light" ? "dark" : "light";
 
         setTheme(newTheme);
@@ -63,7 +53,6 @@ const Navbar = () => {
         document.body.setAttribute("data-theme", newTheme);
 
         storeInSession("theme", newTheme);
-
     }
 
     return (
@@ -74,24 +63,7 @@ const Navbar = () => {
                     <img src={ theme == "light" ? darkLogo : lightLogo } className="w-full" />
                 </Link>
 
-                <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " + ( searchBoxVisibility ? "show" : "hide" )}>
-                    <input 
-                        type="text"
-                        placeholder="Search"
-                        className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
-                        onKeyDown={handleSearch}
-                    />
-
-                    <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
-                </div>
-
                 <div className="flex items-center gap-3 md:gap-6 ml-auto">
-                    <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
-                    onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}
-                    >
-                        <i className="fi fi-rr-search text-xl"></i>
-                    </button>
-
                     <Link to="/editor" className="hidden md:flex gap-2 link">
                         <i className="fi fi-rr-file-edit"></i>
                         <p>Write</p>
