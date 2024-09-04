67af839aeb82cd4369abcc3b98f249d5269417eb
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-09-04 08:16:18 +0530

    Create SearchBar.jsx

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
+const SearchBar = ({ placeholder = "Search", className = "" }) => {
+    const navigate = useNavigate();
+
+    const handleSearch = (e) => {
+        let query = e.target.value;
+
+        if (e.keyCode === 13 && query.length) {
+            navigate(`/search/${query}`);
+        }
+    };
+
+    return (
+        <div className={`relative ${className}`}>
+            <input
+                type="text"
+                placeholder={placeholder}
+                className="w-full bg-grey p-4 pl-6 pr-12 rounded-full placeholder:text-dark-grey"
+                onKeyDown={handleSearch}
+            />
+            <i className="fi fi-rr-search absolute right-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+        </div>
+    );
+};
+
+export default SearchBar;
