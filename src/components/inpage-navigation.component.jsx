278f8819da076e4537287f856ab86a16a568eb3c
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-09-06 10:35:06 +0530

    Update inpage-navigation.component.jsx

--- src/components/inpage-navigation.component.jsx
+++ src/components/inpage-navigation.component.jsx
@@ -3,72 +3,75 @@ import { useState, useRef, useEffect } from "react";
 export let activeTabLineRef;
 export let activeTabRef;
 
-const InPageNavigation = ({ routes, defaultHidden = [ ], defaultActiveIndex = 0, children }) => {
+const InPageNavigation = ({ routes, defaultHidden = [], defaultActiveIndex = 0, children }) => {
 
     activeTabLineRef = useRef();
     activeTabRef = useRef();
 
-    let [ inPageNavIndex, setInPageNavIndex ] = useState(null);
-
-    let [ isResizeEventAdded, setIsResizeEventAdded ] = useState(false);
-    let [ width, setWidth ] = useState(window.innerWidth);
+    let [inPageNavIndex, setInPageNavIndex] = useState(null);
+    let [isResizeEventAdded, setIsResizeEventAdded] = useState(false);
+    let [width, setWidth] = useState(window.innerWidth);
 
     const changePageState = (btn, i) => {
-        
-        let { offsetWidth, offsetLeft } = btn; 
+        let { offsetWidth, offsetLeft } = btn;
 
-        activeTabLineRef.current.style.width = offsetWidth + "px"; 
-        activeTabLineRef.current.style.left = offsetLeft + "px"; 
+        activeTabLineRef.current.style.width = offsetWidth + "px";
+        activeTabLineRef.current.style.left = offsetLeft + "px";
 
         setInPageNavIndex(i);
-
-    }
+    };
 
     useEffect(() => {
-
-        if(width > 766 && inPageNavIndex != defaultActiveIndex){
-            changePageState( activeTabRef.current, defaultActiveIndex )
+        if (width > 766 && inPageNavIndex !== defaultActiveIndex) {
+            changePageState(activeTabRef.current, defaultActiveIndex);
         }
 
-        if(!isResizeEventAdded){
-            window.addEventListener('resize', () => {
-                if(!isResizeEventAdded){
+        if (!isResizeEventAdded) {
+            window.addEventListener("resize", () => {
+                if (!isResizeEventAdded) {
                     setIsResizeEventAdded(true);
                 }
 
                 setWidth(window.innerWidth);
-            })
+            });
         }
-
-    }, [width])
+    }, [width]);
 
     return (
         <>
             <div className=" px-4 text-gray-800 font-semibold py-2 rounded-t border-t border-r border-l -mb-px">
-                
                 {
                     routes.map((route, i) => {
                         return (
-                            <button 
-                            ref={ i == defaultActiveIndex ? activeTabRef : null }
-                            key={i} 
-                            className={"p-4 px-5 capitalize " + ( inPageNavIndex == i ? "text-black " : "text-dark-grey " ) + ( defaultHidden.includes(route) ? " md:hidden " : " " )} 
-                            onClick={(e) => { changePageState(e.target, i) }}
+                            <button
+                                ref={i === defaultActiveIndex ? activeTabRef : null}
+                                key={i}
+                                className={
+                                    "p-4 px-5 capitalize " +
+                                    (inPageNavIndex === i
+                                        ? "text-black border border-white rounded " // Active tab style
+                                        : "text-dark-grey border border-transparent rounded") + // Non-active tab style
+                                    (defaultHidden.includes(route) ? " md:hidden " : "")
+                                }
+                                onClick={(e) => { changePageState(e.target, i); }}
                             >
-                                { route }
+                                {route}
                             </button>
-                        )
+                        );
                     })
                 }
 
-                <hr ref={activeTabLineRef} className="bg-white px-4 text-gray-800 font-semibold py-2 rounded-t border-t border-r border-l -mb-px" />
-
+                {/* The active tab indicator line */}
+                <hr
+                    ref={activeTabLineRef}
+                    className="bg-white px-4 text-gray-800 font-semibold py-2 rounded-t border-t border-r border-l -mb-px"
+                />
             </div>
 
-            { Array.isArray(children) ? children[inPageNavIndex] : children }
-
+            {/* Rendering children based on the current tab */}
+            {Array.isArray(children) ? children[inPageNavIndex] : children}
         </>
-    )
-}
+    );
+};
 
 export default InPageNavigation;
