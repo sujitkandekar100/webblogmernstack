42f6772dba1a288f0404be4402be27e78b873b82
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-09-06 09:44:17 +0530

    Update inpage-navigation.component.jsx

--- src/components/inpage-navigation.component.jsx
+++ src/components/inpage-navigation.component.jsx
@@ -3,63 +3,72 @@ import { useState, useRef, useEffect } from "react";
 export let activeTabLineRef;
 export let activeTabRef;
 
-const InPageNavigation = ({ routes, defaultHidden = [], defaultActiveIndex = 0, children }) => {
+const InPageNavigation = ({ routes, defaultHidden = [ ], defaultActiveIndex = 0, children }) => {
+
     activeTabLineRef = useRef();
     activeTabRef = useRef();
 
-    let [inPageNavIndex, setInPageNavIndex] = useState(null);
-    let [isResizeEventAdded, setIsResizeEventAdded] = useState(false);
-    let [width, setWidth] = useState(window.innerWidth);
+    let [ inPageNavIndex, setInPageNavIndex ] = useState(null);
+
+    let [ isResizeEventAdded, setIsResizeEventAdded ] = useState(false);
+    let [ width, setWidth ] = useState(window.innerWidth);
 
     const changePageState = (btn, i) => {
+        
+        let { offsetWidth, offsetLeft } = btn; 
+
+        activeTabLineRef.current.style.width = offsetWidth + "px"; 
+        activeTabLineRef.current.style.left = offsetLeft + "px"; 
+
         setInPageNavIndex(i);
-    };
+
+    }
 
     useEffect(() => {
-        if (width > 766 && inPageNavIndex !== defaultActiveIndex) {
-            changePageState(activeTabRef.current, defaultActiveIndex);
+
+        if(width > 766 && inPageNavIndex != defaultActiveIndex){
+            changePageState( activeTabRef.current, defaultActiveIndex )
         }
 
-        if (!isResizeEventAdded) {
-            window.addEventListener("resize", () => {
-                if (!isResizeEventAdded) {
+        if(!isResizeEventAdded){
+            window.addEventListener('resize', () => {
+                if(!isResizeEventAdded){
                     setIsResizeEventAdded(true);
                 }
+
                 setWidth(window.innerWidth);
-            });
+            })
         }
-    }, [width]);
+
+    }, [width])
 
     return (
-        <div className="p-8">
-            {/* Horizontal Tab Navigation */}
-            <ul className="grid grid-flow-col text-center text-gray-500 p-1">
-                {routes.map((route, i) => (
-                    <li key={i}>
-                        <a
-                            href={`#${route}`}
-                            ref={i === defaultActiveIndex ? activeTabRef : null}
-                            className={`flex justify-center py-4 transition-all duration-300 ease-in-out
-                                ${inPageNavIndex === i
-                                    ? "bg-white border-l border-t border-r border-gray-100 rounded-tl-lg rounded-tr-lg text-black"
-                                    : "hover:bg-gray-100"} `}
-                            onClick={(e) => {
-                                e.preventDefault();
-                                changePageState(e.target, i);
-                            }}
-                        >
-                            {route}
-                        </a>
-                    </li>
-                ))}
-            </ul>
+        <>
+            <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
+                
+                {
+                    routes.map((route, i) => {
+                        return (
+                            <button 
+                            ref={ i == defaultActiveIndex ? activeTabRef : null }
+                            key={i} 
+                            className={"p-4 px-5 capitalize " + ( inPageNavIndex == i ? "text-black " : "text-dark-grey " ) + ( defaultHidden.includes(route) ? " md:hidden " : " " )} 
+                            onClick={(e) => { changePageState(e.target, i) }}
+                            >
+                                { route }
+                            </button>
+                        )
+                    })
+                }
 
-            {/* Content Box Below Navigation */}
-            <div className="bg-white shadow border border-gray-100 p-8 text-gray-700 rounded-lg -mt-2">
-                {Array.isArray(children) ? children[inPageNavIndex] : children}
+                <hr ref={activeTabLineRef} className="absolute bottom-0 duration-300 rounded-t" />
+
             </div>
-        </div>
-    );
-};
+
+            { Array.isArray(children) ? children[inPageNavIndex] : children }
+
+        </>
+    )
+}
 
 export default InPageNavigation;
