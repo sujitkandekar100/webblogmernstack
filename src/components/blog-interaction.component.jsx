36b18d40e08c7f25c5b20480e32b7fe10ef40f41
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-09-08 15:08:11 +0530

    Update blog-interaction.component.jsx

--- src/components/blog-interaction.component.jsx
+++ src/components/blog-interaction.component.jsx
@@ -6,101 +6,119 @@ import { Toaster, toast } from "react-hot-toast";
 import axios from "axios";
 
 const BlogInteraction = () => {
+    const {
+        blog,
+        blog: { _id, title, blog_id, activity, activity: { total_likes, total_comments }, author: { personal_info: { username: author_username } } },
+        setBlog,
+        islikedByUser,
+        setLikedByUser,
+        setCommentsWrapper
+    } = useContext(BlogContext);
 
-    let { blog, blog: { _id, title, blog_id, activity, activity: { total_likes, total_comments }, author: { personal_info: { username: author_username } }  }, setBlog, islikedByUser, setLikedByUser, setCommentsWrapper } = useContext(BlogContext);
-
-    let { userAuth: { username, access_token } } = useContext(UserContext);
+    const { userAuth: { username, access_token } } = useContext(UserContext);
 
     useEffect(() => {
-
-        if( access_token ){
-            // make request to server to get like information
+        if (access_token) {
             axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/isliked-by-user", { _id }, {
                 headers: {
                     'Authorization': `Bearer ${access_token}`
                 }
             })
             .then(({ data: { result } }) => {
-                setLikedByUser(Boolean(result))
+                setLikedByUser(Boolean(result));
             })
             .catch(err => {
                 console.log(err);
-            })
+            });
         }
-
-    }, [])
+    }, [access_token, _id, setLikedByUser]);
 
     const handleLike = () => {
-
-        if(access_token){
-            // like the blog
-            setLikedByUser(preVal => !preVal);
-
-            !islikedByUser ? total_likes++ : total_likes--;
-
-            setBlog({ ...blog, activity: { ...activity, total_likes } });
+        if (access_token) {
+            const updatedLikes = islikedByUser ? total_likes - 1 : total_likes + 1;
+            setLikedByUser(prevVal => !prevVal);
+            setBlog({ ...blog, activity: { ...activity, total_likes: updatedLikes } });
 
             axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/like-blog", { _id, islikedByUser }, {
-                headers: { 
+                headers: {
                     'Authorization': `Bearer ${access_token}`
                 }
             })
             .then(({ data }) => {
                 console.log(data);
             })
-            .catch(err =>{
+            .catch(err => {
                 console.log(err);
             });
-            
-        } 
-        else{
-            // not logged in
-            toast.error("please login to like this blog");
+        } else {
+            toast.error("Please login to like this blog");
         }
+    };
 
-    }
+    const handleShare = () => {
+        navigator.clipboard.writeText(location.href);
+        toast.success("Link copied to clipboard!");
+    };
 
     return (
         <>
             <Toaster />
             <div className="max-w-3xl mx-auto p-6 bg-white rounded-3xl shadow-lg border border-gray-200">
-                <hr className="my-4 h-1 bg-gray-300 rounded-full border-0" />
-
-                <div className="flex gap-6 justify-between">
+                <div className="flex flex-col md:flex-row md:justify-between gap-6">
+                    
+                    {/* Like and Comment Section */}
                     <div className="flex gap-3 items-center">
                         <button
                             onClick={handleLike}
-                            className={"w-10 h-10 rounded-full flex items-center justify-center " + ( islikedByUser ? "bg-red/20 text-red" : "bg-gray-200" )}
+                            className={`w-12 h-12 rounded-full flex items-center justify-center ${islikedByUser ? "bg-[#24a0ed]/20 text-[#24a0ed]" : "bg-gray-200"}`}
                         >
-                            <i className={"fi " + ( islikedByUser ? "fi-sr-heart" : "fi-rr-heart" )}></i>
+                            <i className={`fi ${islikedByUser ? "fi-ss-arrow-circle-up" : "fi-rs-arrow-circle-up"}`}></i>
                         </button>
-                        <p className="text-xl text-dark-gray">{ total_likes }</p>
+                        <p className="text-lg text-gray-700">{total_likes}</p>
 
                         <button
-                            onClick={() => setCommentsWrapper(preVal => !preVal)}
-                            className="w-10 h-10 rounded-full flex items-center justify-center bg-gray-200"
+                            onClick={() => setCommentsWrapper(prevVal => !prevVal)}
+                            className="w-12 h-12 rounded-full flex items-center justify-center bg-gray-200"
                         >
-                            <i className="fi fi-rr-comment-dots"></i>
+                            <i className="fi fi-rr-comment-alt-dots"></i>
                         </button>
-                        <p className="text-xl text-dark-gray">{ total_comments }</p>
+                        <p className="text-lg text-gray-700">{total_comments}</p>
                     </div>
 
+                    {/* Edit and Share Section */}
                     <div className="flex gap-6 items-center">
-                        {
-                            username === author_username ? 
-                            <Link to={`/editor/${blog_id}`} className="underline hover:text-purple">Edit</Link> : ""
-                        }
-
-                        <Link to={`https://twitter.com/intent/tweet?text=Read ${title}&url=${location.href}`}>
-                            <i className="fi fi-brands-twitter text-xl hover:text-twitter"></i>
-                        </Link>
+                        {username === author_username && (
+                            <Link to={`/editor/${blog_id}`} className="underline text-[#24a0ed] hover:text-[#24a0ed]">
+                                Edit
+                            </Link>
+                        )}
+                        <button
+                            onClick={handleShare}
+                            className="w-12 h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-[#24a0ed]/20 hover:text-[#24a0ed]"
+                        >
+                            <i className="fi fi-rr-share-square"></i>
+                        </button>
+                        <a
+                            href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(location.href)}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="w-12 h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-[#24a0ed]/20 hover:text-[#24a0ed]"
+                        >
+                            <i className="fi fi-brands-linkedin text-xl"></i>
+                        </a>
+                        <a
+                            href={`https://twitter.com/intent/tweet?text=Read ${encodeURIComponent(title)}&url=${encodeURIComponent(location.href)}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="w-12 h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-[#24a0ed]/20 hover:text-[#24a0ed]"
+                        >
+                            <i className="fi fi-brands-twitter-alt text-xl"></i>
+                        </a>
                     </div>
                 </div>
-
-                <hr className="my-4 h-1 bg-gray-300 rounded-full border-0" />
             </div>
         </>
     );
-}
+};
 
 export default BlogInteraction;
