24b6402026abc064c769ebd4105e2865a5e9b9bd
Sujit Kandekar <sujitkandekar100@gmail.com>
2024-09-25 20:41:11 +0530

    Update blog-post.component.jsx

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -5,7 +5,7 @@ const BlogPostCard = ({ content, author }) => {
         <Link to={`/blog/${id}`} className="shadow-md rounded-lg flex flex-col text-left text-[14px] min-h-[100px] max-w-[250px]" >
             {/* Image Section */}
             <img
-                className="w-full h-40 sm:h-48 rounded-t-lg object-cover"
+                className="w-full h-50 sm:h-48 rounded-t-lg object-cover"
                 loading="lazy"
                 alt={title}
                 src={banner}
